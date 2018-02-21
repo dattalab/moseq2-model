13401fca906e6989f6cf505ce744004093a18e6e
jmarkow <jmarkow@cns.bu.edu>
2018-02-21 13:36:33 -0500

    Cleanup of env vars in cli

--- scripts/moseq2_model_kube_down.sh
+++ scripts/moseq2_model_kube_down.sh
@@ -3,11 +3,29 @@
 
 hash gcloud 2>/dev/null || { echo >&2 "gcloud not installed.  Aborting."; exit 1; }
 
-if [ -z "${KINECT_GKE_CLUSTER_NAME}" ]; then
+#######################################
+# Run extraction
+# Environment variables (optional, will override defaults for options):
+#   MOSEQ2_GKE_CLUSTER_NAME=cluster-name
+#
+# Arguments:
+#   -c|--cluster-name (string): name of cluster"
+#   -a|--authorize (flag): run gce authentication
+#
+# Returns:
+#   None
+#
+# Example (tear down a cluster):
+#   moseq2_model_kube_down --cluster-name kinect-modeling-jmarkow
+#
+#######################################
+
+
+if [ -z "${MOSEQ2_GKE_CLUSTER_NAME}" ]; then
 	CLUSTERNAME="test-cluster"
 else
-	echo "Setting cluster name to ${KINECT_GKE_CLUSTER_NAME}"
-	CLUSTERNAME="${KINECT_GKE_CLUSTER_NAME}"
+	echo "Setting cluster name to ${MOSEQ2_GKE_CLUSTER_NAME}"
+	CLUSTERNAME="${MOSEQ2_GKE_CLUSTER_NAME}"
 fi
 
 AUTHORIZE=false
@@ -17,6 +35,15 @@ do
 key="$1"
 
 case $key in
+    -h|--help)
+    echo "Usage: $0 -i [options]";
+    echo "";
+    echo "Arguments:";
+    echo "  -c|--cluster-name (string): name of cluster"
+    echo "  -a|--authorize (flag): run gce authentication"
+    echo "";
+    exit 1;
+    ;;
     -c|--cluster-name)
     CLUSTERNAME="$2"
     shift # past argument
