d19a48b419fc70aab7bd85de711ef68bc1516d02
Winthrop Gillis <win.gillis@gmail.com>
2019-01-24 17:20:12 +0000

    remove print statements from the training function

--- moseq2_model/train/util.py
+++ moseq2_model/train/util.py
@@ -29,8 +29,6 @@ def train_model(model, num_iter=100, save_every=1, ncpus=1, cli=False, **kwargs)
             for seq_itr in range(len(seq_list)):
                 seq_list[seq_itr] = np.append(np.repeat(-5, model.nlags), seq_list[seq_itr])
             labels.append(seq_list)
-            print('number of unique labels', np.unique(seq_list[0]).size)
-            print('log likelihood', log_likelihoods[-1])
         if save_progress is not None and ((itr + 1) % save_progress == 0 or itr == 0):
             # move around the checkpoints
             if os.path.exists(filename):
