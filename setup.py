54e9f7a9951f24c6ac9f8533a32db8feb5a6f507
jmarkow <jmarkow@cns.bu.edu>
2019-02-06 14:13:08 -0500

    Fix for pip >=19

--- setup.py
+++ setup.py
@@ -40,8 +40,8 @@ setup(
     python_requires='>=3.6',
     install_requires=['future', 'h5py', 'click', 'numpy==1.14.5',
                       'pyhsmm', 'autoregressive', 'joblib==0.11',
-                      'hdf5storage', 'ruamel.yaml>=0.15.0', 'tqdm'],
-    dependency_links=['git+https://github.com/mattjj/pybasicbayes.git@master#egg=pybasicbayes-1',
-                      'git+https://github.com/mattjj/pyhsmm-autoregressive.git@master#egg=autoregressive-1'],
+                      'hdf5storage', 'ruamel.yaml>=0.15.0', 'tqdm',
+                      'pybasicbayes @ https://github.com/mattjj/pybasicbayes.git@master',
+                      'pyhsmm-autoregressive @ https://github.com/mattjj/pyhsmm-autoregressive.git@master'],
     entry_points={'console_scripts': ['moseq2-model = moseq2_model.cli:cli']},
 )
