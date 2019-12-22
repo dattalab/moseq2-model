8cf7d1b1f32a618bde9a8cca159b655a8d505ac9
Ayman Zeine <a.zeine.96@gmail.com>
2019-12-22 17:57:07 -0500

    GUI verbose flag for clean exit

--- moseq2_model/gui.py
+++ moseq2_model/gui.py
@@ -377,4 +377,5 @@ def learn_model_command(input_file, dest_file, config_file, index, hold_out, nfo
 
     save_dict(filename=str(dest_file), obj_to_save=export_dict)
 
-    return img_path
+    if verbose:
+        return img_path
